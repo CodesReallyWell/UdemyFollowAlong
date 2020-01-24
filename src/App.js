fb5df2a132c9b3b51be0863467b79c6a9b008842
CodesReallyWell <liamanewton@gmail.com>
2020-01-24 12:36:02 -0600

    New lecture over conditional displaying of elements

--- src/App.js
+++ src/App.js
@@ -9,18 +9,20 @@ const App = props => {
           { name: 'Dave', age: 52},
           { name: 'Liam', age: 24}
         ],
-        otherState: 'some value'
+        otherState: 'some value',
+        showPersons: true
       })
 
       const switchHandler = () => {
         // console.log('Was Clicked!')
         // DO NOT DO THIS this.state.persons[0].name = 'Leon'
         // with this method this replaces the old state
-        setPersonsState({persons: [
-                          { name: 'sdfax', age: 281},
-                          { name: 'Dane', age: 522},
-                          { name: 'Leon', age: 256}
-                        ],
+        setPersonsState({
+          persons: [
+            { name: 'sdfax', age: 281},
+            { name: 'Dane', age: 522},
+            { name: 'Leon', age: 256}
+          ]
         })
     }
 
@@ -46,17 +48,47 @@ const App = props => {
     
     //console.log(personState, otherState)
 
+    const togglePersonsHandler = () => {
+      const doesShow = personState.showPersons
+      setPersonsState({
+        persons: [
+          { name: 'Max', age: 28},
+          { name: 'Dave', age: 52},
+          { name: 'Liam', age: 24}
+        ],
+        showPersons: !doesShow})
+    }
+
+    const deletePersonHandler = (personIndex) => {
+      //const persons = personState.persons.slice() // create new array so don't del old data
+      const persons=[...personState.persons] // using spread method more modern
+      persons.splice(personIndex, 1)
+      setPersonsState({persons: persons,
+                      showPersons: true})
+    }
+
+    let persons = null
+
+    if (personState.showPersons){
+      persons = (
+        <div>
+          {personState.persons.map((person, index) => {
+            return <Person 
+            click={() => deletePersonHandler(index)}
+            name={person.name} 
+            age={person.age}/>
+          })}
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hello World</h1>
         <button 
         style={style}
-        onClick={switchHandler}>Switch</button>
-        <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
-        <Person name={personState.persons[1].name} age={personState.persons[1].age}
-        click={switchHandler}
-        changed={nameChangeHandler}>My Hobbies: Racing</Person>
-        <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
+        onClick={togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
 
